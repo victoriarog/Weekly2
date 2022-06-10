39237011793d9ffb12d56c1eb6bfc3d1c24c052b
Vicka <vicrogatinskaya@gmail.com>
2022-06-10 15:37:44 +0200

    more to disttt fixesss

--- dist/scenes/HomePage/components/Hero.tsx
+++ dist/scenes/HomePage/components/Hero.tsx
@@ -3,7 +3,7 @@ import s from "./Hero.module.css";
 export const Hero = () => {
   return (
     <section className={s.container}>
-      <img className={s.image} src="bg.jpg" alt="" />
+      <img className={s.image} src="bg.jpg" height="1080" width="1080" alt="" />
       <div className={s.center}>
         <h1 className={s.title}>Star fighter</h1>
         <a className={s.button} href="/game">
